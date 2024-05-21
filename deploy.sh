35f863aa917d32970321565b14da2a6931ed98e4
ampacheco <ampacheco@users.noreply.github.com>
2024-05-20 21:32:07 -0400

    parametrizado

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,20 @@
-az group create --name 000_JS-AKS-RG --location eastus
-az aks create -n JS-AKS -g 000_JS-AKS-RG --network-plugin azure --enable-managed-identity -a ingress-appgw --appgw-name myApplicationGateway --appgw-subnet-cidr "10.225.0.0/16" --generate-ssh-keys --node-count 1
-appGatewayId=$(az aks show -n 000_JS-AKS-RG -g myResourceGroup -o tsv --query "addonProfiles.ingressApplicationGateway.config.effectiveApplicationGatewayId")
-appGatewayId=$(az aks show -n JS-AKS -g 000_JS-AKS-RG -o tsv --query "addonProfiles.ingressApplicationGateway.config.effectiveApplicationGatewayId")
-agicAddonIdentity=$(az aks show -n JS_AKS -g 000_JS-AKS-RG -o tsv --query "addonProfiles.ingressApplicationGateway.identity.clientId")
-agicAddonIdentity=$(az aks show -n JS-AKS -g 000_JS-AKS-RG -o tsv --query "addonProfiles.ingressApplicationGateway.identity.clientId")
+resource_group_name=000_JS-AKS-RG
+cluster_name=JS-AKS
+
+
+az group create --name ${resource_group_name} --location eastus
+az aks create -n ${cluster_name} -g ${resource_group_name} --network-plugin azure --enable-managed-identity -a ingress-appgw --appgw-name myApplicationGateway --appgw-subnet-cidr "10.225.0.0/16" --generate-ssh-keys --node-count 1
+
+# Get application gateway id from AKS addon profile
+appGatewayId=$(az aks show -n ${cluster_name} -g ${resource_group_name} -o tsv --query "addonProfiles.ingressApplicationGateway.config.effectiveApplicationGatewayId")
+
+# Get Application Gateway subnet id
+appGatewaySubnetId=$(az network application-gateway show --ids $appGatewayId -o tsv --query "gatewayIPConfigurations[0].subnet.id")
+
+# Get AGIC addon identity
+agicAddonIdentity=$(az aks show -n ${cluster_name} -g ${resource_group_name} -o tsv --query "addonProfiles.ingressApplicationGateway.identity.clientId")
+
+# Assign network contributor role to AGIC addon identity to subnet that contains the Application Gateway
+az role assignment create --assignee $agicAddonIdentity --scope $appGatewaySubnetId --role "Network Contributor"
+
 az aks get-credentials -n JS-AKS -g 000_JS-AKS-RG
